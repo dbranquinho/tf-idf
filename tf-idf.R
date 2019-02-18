feed1dfa7c3fa68d91cda6c07c86f70771eaaaca
Delermando Branquinho Filho <delermando@gmail.com>
2019-02-18 14:15:04 -0300

    bug fix

--- tf-idf.R
+++ tf-idf.R
@@ -45,22 +45,36 @@ book_words <- myDataset %>%
 book_words <- book_words[order(book_words$file),]
 
 names(book_words) <- c("file","word","f")
+conta <- function(x) {
+        y <- 0
+        for(i in 1:length(x)) {
+              y <- y+1  
+        }
+        return(y)
+}
 
-total_words <- book_words %>% group_by(word) %>% summarize(total = sum(f))
-book_words <- left_join(book_words, total_words, by = "word")
+bind_tfidf <- function(files,f,n) {
+        nfiles <- length(unique(book_words$file))
+        tf <- 1+log2(f)
+        idf <- log2(4/n)
+        tf_idf <- tf * idf
+        book_words$df <- tf
+        book_words$idf <- idf
+        book_words$tf_idf <- tf_idf
+}
+
+total_file <- book_words
+total_file <- aggregate(total_file,by = list(total_file$word),conta)
+
+total_file <- total_file[,-2:-3]
+names(total_file) <- c("word","f")
+book_words <- left_join(book_words, total_file, by = "word")
+names(book_words) <- c("file","word","n","f")
 head(book_words)
 
 
 # Create matrix with TF-IDF
-book_words <- book_words %>% bind_tf_idf(term = word, document = file, n = n) 
-book_words <- as.data.table(book_words)
-
-book_words$class <- myDataset[match(book_words$file,myDataset$file),"class"]
-
-setkey(book_words,file,word,class)
-
-write.csv(book_words,file = "tf-idf.csv")
-book_words
+book_words$tf_idf <- bind_tfidf(book_words$file,book_words$f,book_words$n)
+print(book_words)
 
 
-log(4/2)
